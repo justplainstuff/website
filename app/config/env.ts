16316308d9524f0ae975f4ab16d85cfd21c60d13
Josef Erben <josef@erben.systems>
2024-09-24 09:40:27 +0200

    fix: add healthchecks

--- app/config/env.ts
+++ app/config/env.ts
@@ -11,6 +11,5 @@ export default defineEnv((z) =>
     SMTP_PASS: z.string(),
     CF_TURNSTILE_SECRET: z.string(),
     CF_TURNSTILE_SITEKEY: z.string(),
-    ADMIN_PASSWORD: z.string(),
-  }),
+  })
 );
